f3a90625ba8e280ea75bed32b909e733a8782f7e
anbhatna <ankita.paris@hughes.com>
2019-06-28 12:28:14 -0400

    Final Commit

---
+++ lib/my_new_test_module02.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+ANSIBLE_METADATA = {
+        'metadata_version' : '1.1',
+        'status' : ['preview'],
+        'supported_by' : 'community'
+        }
+
+DOCUMENTATION = '''
+---
+Build me later
+'''
+
+EXAMPLES = '''
+write some examples later
+'''
+
+RETURN = '''
+original_message:
+    description: Original name param that was passed in
+    type: str
+message:
+    description: The outut message that the sample module generates
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+
+def run_module():
+    module_args = dict (
+            name=dict(type='str', required=True),
+            new=dict(type='bool', required=False, default=False),
+            turtle01=dict(type='str', required=False, default='box')
+            )
+
+    result = dict(
+            changed=False,
+            original_message='',
+            message=''
+            )
+    module = AnsibleModule(
+            argument_spec=module_args,
+            supports_check_mode=True            
+            )
+
+    if module.check_mode:
+        return result
+
+    result['original_message'] =  module.params['name']
+    result['message']= 'hello ' + module.params['name'] + ' pleasure to meet you!'
+
+    if module.params['turtle01'] != 'box':
+        result['message'] = result['message'] + ' Wow your ' + module.params[turtle01] + ' is a cutie' 
+
+    if module.params['new']:
+        result['changed'] = True
+
+    if module.params['name'].lower() == 'hughes':
+        result['message'] += 'And you work at Hughes!!! What a great place to work!!'
+
+    if module.params['name'] == 'fail me':
+        module.fail_jason(msg='You requested the module to fail',  **result)
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ lib/z.py
@@ -0,0 +1,4 @@
+def main():
+    print("Uh ho, This just runs")
+
+main()
